16c6598b5b3ae8d2f8b8ce9b317e45d34eae19aa
mrbrique <locus.chan.dev@gmail.com>
2025-03-10 12:06:20 +0000

    add digit class

---
+++ src/app/model/digit.spec.ts
@@ -0,0 +1,7 @@
+import { Digit } from './digit';
+
+describe('Digit', () => {
+  it('should create an instance', () => {
+    expect(new Digit()).toBeTruthy();
+  });
+});
---
+++ src/app/model/digit.ts
@@ -0,0 +1,34 @@
+export class Digit {
+
+    _digit: number;
+
+    _pow: number;
+
+    constructor(digit: number, pow: number) {
+
+        if (!Number.isInteger(digit)) {
+            throw new Error('digit must be integer');
+        }
+
+        if (digit < 0 || digit > 9) {
+            throw new Error('digit must be between 0 and 9');
+        }
+
+
+        if (!Number.isInteger(pow)) {
+            throw new Error('pow must be integer');
+        }
+
+        this._digit = digit;
+        this._pow = pow;
+    }
+
+
+    get digit() {
+        return this._digit;
+    }
+
+    get pow() {
+        return this._pow;
+    }
+}
