6dd1a44bb2044606538fa9c5e8ffa221b17c68d8
mrbrique <locus.chan.dev@gmail.com>
2025-03-13 11:44:46 +0000

    update number-block test case

--- src/app/component/number-block/number-block.component.spec.ts
+++ src/app/component/number-block/number-block.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NumberBlockComponent } from './number-block.component';
+import { Digit } from '../../model/digit';
 
 describe('NumberBlockComponent', () => {
   let component: NumberBlockComponent;
@@ -10,10 +11,11 @@ describe('NumberBlockComponent', () => {
     await TestBed.configureTestingModule({
       imports: [NumberBlockComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(NumberBlockComponent);
     component = fixture.componentInstance;
+    component.digit = new Digit(null, 0, 0);
     fixture.detectChanges();
   });
 
