59f8e8dda053273dfc94d1b7066336a921767575
mrbrique <locus.chan.dev@gmail.com>
2025-03-14 15:13:44 +0000

    add color name to number-card

--- src/app/component/number-nook/number-nook.component.ts
+++ src/app/component/number-nook/number-nook.component.ts
@@ -2,10 +2,11 @@ import { Component, Input } from '@angular/core';
 import { Digit } from '../../model/digit';
 import { NumberCardComponent } from '../number-card/number-card.component';
 import { NumSequence } from '../../model/num-sequence';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'number-nook',
-  imports: [NumberCardComponent],
+  imports: [CommonModule, NumberCardComponent],
   templateUrl: './number-nook.component.html',
   styleUrl: './number-nook.component.scss'
 })
