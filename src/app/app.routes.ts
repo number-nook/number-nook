f04691e0289b652c97d7759c644367da2c0f6996
mrbrique <locus.chan.dev@gmail.com>
2025-03-17 14:40:15 +0000

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TestCardComponent } from './component/test-card/test-card.component';
+import { NumberNookComponent } from './component/number-nook/number-nook.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: NumberNookComponent },
+    { path: 'test', component: TestCardComponent }
+];
