7334cbbf98743dee4b731872a6b71f1bd3b9753c
mrbrique <locus.chan.dev@gmail.com>
2025-03-17 01:50:10 +0000

    implement NumSegment

--- src/app/model/num-segment.ts
+++ src/app/model/num-segment.ts
@@ -1,2 +1,28 @@
+import { Digit } from "./digit";
+import { NumSequence } from "./num-sequence";
+
 export class NumSegment {
+
+    static readonly SIZE: number = 3;
+
+    private _numSequence: NumSequence;
+
+    private _digits: Digit[] = [];
+
+    constructor(numseq: NumSequence, p: number, q: number) {
+        this._numSequence = numseq;
+        for (let i = p; i <= q; i++) {
+            this._digits.push(new Digit(this, numseq.sequence[i], numseq.sequence.length - 1 - i));
+        }
+
+    }
+
+    get numSequence() {
+        return this._numSequence;
+    }
+
+    get digits(): Digit[] {
+        return this._digits;
+    }
+
 }
