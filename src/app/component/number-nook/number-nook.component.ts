4b8be5dc38cc69ee54458f4202f9758de02ff3ea
mrbrique <locus.chan.dev@gmail.com>
2025-03-17 16:04:27 +0000

    add number input to number-nook

--- src/app/component/number-nook/number-nook.component.ts
+++ src/app/component/number-nook/number-nook.component.ts
@@ -12,18 +12,48 @@ import { CommonModule } from '@angular/common';
 })
 export class NumberNookComponent {
 
+  static readonly INIT = 0;
+  static readonly MIN = 0;
+  static readonly MAX = 999999999999;
+
   private _numSequence: NumSequence;
 
+  constructor() {
+    this.integer = NumberNookComponent.INIT;
+  }
+
+  onNumberChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    let int = parseInt(inputElement.value);
+
+    if (int > NumberNookComponent.MAX || int < NumberNookComponent.MIN) {
+      event.preventDefault();
+      int =
+        Math.max(NumberNookComponent.MIN, Math.min(int, NumberNookComponent.MAX))
+      inputElement.value = int + '';
+    }
+
+    this.integer = int;
+  }
 
   @Input()
   set integer(int: number) {
-
     this._numSequence = new NumSequence(Math.trunc(int));
-
   }
 
   get numSequence() {
     return this._numSequence;
   }
 
+  get min() {
+    return NumberNookComponent.MIN;
+  }
+
+  get max() {
+    return NumberNookComponent.MAX;
+  }
+
+  get init() {
+    return NumberNookComponent.INIT;
+  }
 }
