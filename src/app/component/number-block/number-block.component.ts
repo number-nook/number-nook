42f872226224c3729fe572cc49c61b17660e65f9
mrbrique <locus.chan.dev@gmail.com>
2025-03-18 13:59:45 +0000

    fix number nook dp to be 2 lines

--- src/app/component/number-block/number-block.component.ts
+++ src/app/component/number-block/number-block.component.ts
@@ -31,7 +31,12 @@ export class NumberBlockComponent {
   }
 
   get decimalPlace() {
-    return this.numScaleService.decimalPlace(this.digit.pow);
+    let dp = this.numScaleService.decimalPlace(this.digit.pow)?.split(' ');
+    if (dp!.length < 2) {
+      // assure it takes up 2 lines
+      dp?.unshift('');
+    }
+    return dp!.join('<br />');
   }
 
 }
